3f8fb1334fab34b003b775859f37b59cf65beb69
CrisPih <152130690+CrisPih@users.noreply.github.com>
2023-11-28 20:52:41 -0500

    Update Utilitaria.java

--- src/main/java/ec/edu/espol/juego/Utilitaria.java
+++ src/main/java/ec/edu/espol/juego/Utilitaria.java
@@ -9,5 +9,37 @@ package ec.edu.espol.juego;
  * @author crisp
  */
 public class Utilitaria {
+    private String nombre;
+    private ArrayList<Ficha> mano;
+
+    public Jugador(String nombre, ArrayList<Ficha> mano) {
+        this.nombre = nombre;
+        this.mano = mano;
+    }
+
+    public Ficha getFicha(int i) {
+        if (i >= 0 && i < mano.size()) {
+            return mano.get(i);
+        }
+        return null;
+    }
+
+    public ArrayList<Ficha> getMano() {
+        return mano;
+    }
+
+    public void removerFicha(Ficha f) {
+    mano.remove(f);
+    }
+        
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void imprimirMano() {
+        System.out.println(mano);
+    }
+
+
     
 }
