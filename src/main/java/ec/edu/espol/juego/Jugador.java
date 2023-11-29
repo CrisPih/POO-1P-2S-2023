f4e5eb68f70bd1044a441e7208be2b7713f8bb8f
CrisPih <152130690+CrisPih@users.noreply.github.com>
2023-11-28 20:49:18 -0500

    Update Jugador.java

--- src/main/java/ec/edu/espol/juego/Jugador.java
+++ src/main/java/ec/edu/espol/juego/Jugador.java
@@ -9,5 +9,35 @@ package ec.edu.espol.juego;
  * @author crisp
  */
 public class Jugador {
+    private String nombre;
+    private ArrayList<Ficha> mano;
+
+    public Jugador(String nombre, ArrayList<Ficha> mano) {
+        this.nombre = nombre;
+        this.mano = mano;
+    }
+
+    public Ficha getFicha(int i) {
+        if (i >= 0 && i < mano.size()) {
+            return mano.get(i);
+        }
+        return null;
+    }
+
+    public ArrayList<Ficha> getMano() {
+        return mano;
+    }
+
+    public void removerFicha(Ficha f) {
+    mano.remove(f);
+    }
+        
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void imprimirMano() {
+        System.out.println(mano);
+    }
     
 }
