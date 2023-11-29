1d8ea37950af3dafa538d204be90e0272ef8c0be
CrisPih <mpihuave@espol.edu.com>
2023-11-28 20:31:15 -0500

    FichaComodin

--- src/main/java/ec/edu/espol/juego/FichaComodin.java
+++ src/main/java/ec/edu/espol/juego/FichaComodin.java
@@ -8,28 +8,23 @@ package ec.edu.espol.juego;
  *
  * @author crisp
  */
-public class Ficha {
-    
-    protected int lado1;
-    protected int lado2;
+public class FichaComodin extends Ficha {
+    public FichaComodin(){
+        super(-1,-1);
+    }
 
-    public Ficha(int lado1, int lado2) {
+    public void setLado1(int lado1) {
         this.lado1 = lado1;
+    }
+
+    public void setLado2(int lado2) {
         this.lado2 = lado2;
     }
 
-    public int getLado1() {
-        return lado1;
-    }
-
-    public int getLado2() {
-        return lado2;
-    }
-    
     @Override
     public String toString(){
-        return this.getLado1()+":"+this.getLado2();
+        return "*"+super.toString()+"*";
     }
-        
+ 
     
 }
